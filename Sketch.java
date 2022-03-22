f26cc4a344ef3616fa267f6cb7bf27f3800f3df0
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-03-22 10:00:24 -0400

    Commit

--- Sketch.java
+++ Sketch.java
@@ -15,6 +15,7 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
+    
   public void setup() {
     background(210, 255, 173);
   }
@@ -23,13 +24,39 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	  for (int lineX = width / 20; lineX < width / 2; lineX += width / 20) {
+      line(lineX, 0, lineX, width / 2);
+    }
+    for (int lineY = width / 20; lineY < width / 2; lineY += width / 20) {
+      line(0, lineY, width/2, lineY);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    for (int circleX = width/2 + width/16; circleX < width - width/16; circleX += width/11){
+      ellipse(circleX, height/2 - width/16, height/16, height/16);
+    }
+    for (int circleX = width/2 + width/16; circleX < width - width/16; circleX += width/11){
+      ellipse(circleX, (height/2 - height/16 - height/11), height/16, height/16);
+    }
+    for (int circleX = width/2 + width/16; circleX < width - width/16; circleX += width/11){
+      ellipse(circleX, height/2 - height/16 - height/11*2, height/16, height/16);
+    }
+    for (int circleX = width/2 + width/16; circleX < width - width/16; circleX += width/11){
+      ellipse(circleX, height/2 - height/16 - height/11*3, height/16, height/16);
+    }
+    for (int circleX = width/2 + width/16; circleX < width - width/16; circleX += width/11){
+      ellipse(circleX, height/2 - height/16 - height/11*4, height/15, height/15);
+    }
+
+    for (int gradientY = height/2; gradientY < height; gradientY ++){
+      for (int gradientX = 0; gradientX < width/2; gradientX++){
+        stroke(gradientX, gradientX, gradientX);
+        point(gradientX, gradientY);
+      }
+    }
+
+
+ 
+	
   }
   
   // define other methods down here.
