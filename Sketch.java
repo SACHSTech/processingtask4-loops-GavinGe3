7e1590c925c91a89cf72d5122f159ab1204e336b
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-03-24 09:06:01 -0400

    Commit

--- Sketch.java
+++ Sketch.java
@@ -8,22 +8,27 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
-    
+  
   public void setup() {
-    background(210, 255, 173);
+    background(255, 255, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
+  
   public void draw() {
+    
+    stroke(0,0,0);
+
+    // Draw 10x10 Grid
 	  for (int lineX = width / 20; lineX < width / 2; lineX += width / 20) {
       line(lineX, 0, lineX, width / 2);
     }
@@ -31,19 +36,26 @@ public class Sketch extends PApplet {
       line(0, lineY, width/2, lineY);
     }
 
-    for (int circleX = width/2 + width/16; circleX < width - width/16; circleX += width/11){
-      for (int circleY = width/16; circleY < width/2 - width/16; circleY += width/11){
+    // Draw 5x5 circles
+    for (int circleX = width/2 + width/16; circleX < width - width/16; circleX += width/10.7){
+      for (int circleY = width/16; circleY < width/2 - width/16; circleY += width/10.7){
         ellipse(circleX, circleY, height/16, height/16);
       }
     }
     
-    for (int gradientY = height/2; gradientY < height; gradientY ++){
+  
+    // Draw Gradient
+    for (int gradientY = height/2; gradientY < height; gradientY ++) {
       for (int gradientX = 0; gradientX < width/2; gradientX++){
-        stroke(gradientX, gradientX, gradientX);
-        point(gradientX, gradientY);
+        for (int strokeColor = 0; strokeColor < 256; strokeColor += 255/ width/2){
+          stroke(strokeColor, strokeColor, strokeColor);
+          point(gradientX, gradientY);
+        }
       }
     }
 
+    // Draw Flower
+    stroke(0,0,0);
     int flowerCircleX = width / 2 + width / 4;
     int flowerCircleY = height / 2 + height / 4;
     translate(flowerCircleX, flowerCircleY);
