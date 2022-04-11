f07127a18aa80c256b5ac72c31827bbc05d9f935
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-04-11 08:46:42 -0400

    Cool

--- Sketch.java
+++ Sketch.java
@@ -16,11 +16,11 @@ public class Sketch extends PApplet {
   }
 
   // Declare initial x value for gradient lines
-  float gradientLineX = 0;
+  float fltGradientintLineX = 0;
 
   // Declare count variable for Grid lines
-  int countX = 0;
-  int countY = 0;
+  int intCountX = 0;
+  int intCountY = 0;
 
   public void draw() {
 
@@ -29,42 +29,42 @@ public class Sketch extends PApplet {
     strokeWeight(3);
 
     // Quadrant 1: Draw 10x10 Grid Horizontal Lines
-	  for (int lineX = width / 20; lineX < width / 2; lineX += width / 20) {
-      if (countX < 9){
-        line(lineX, 0, lineX, width / 2);
-        countX++;
+	  for (int intLineX = width / 20; intLineX < width / 2; intLineX += width / 20) {
+      if (intCountX < 9){
+        line(intLineX, 0, intLineX, width / 2);
+        intCountX++;
       }
     }
 
     // Quadrant 1: Draw 10x10 Grid Vertical Lines
-    for (int lineY = height / 20; lineY < height / 2; lineY += height / 20) {
-      if (countY < 9){
-        line(0, lineY, width / 2, lineY);
-        countY++;
+    for (int intLineY = height / 20; intLineY < height / 2; intLineY += height / 20) {
+      if (intCountY < 9){
+        line(0, intLineY, width / 2, intLineY);
+        intCountY++;
       }
     }
 
     // Quadrant 2: Draw 5x5 circles
     fill(212, 15, 125);
-    for (int circleX = width / 2 + width / 16; circleX < width - width / 16; circleX += width / 10.7){
-      for (int circleY = height / 16; circleY < height / 2 - height / 16; circleY += height / 10.7){
-        ellipse(circleX, circleY, height / 16, height / 16);
+    for (int intCircleX = width / 2 + width / 16; intCircleX < width - width / 16; intCircleX += width / 10.7){
+      for (int intCircleY = height / 16; intCircleY < height / 2 - height / 16; intCircleY += height / 10.7){
+        ellipse(intCircleX, intCircleY, height / 16, height / 16);
       }
     }
     
     // Quadrant 3: Draw Gradient 
-    for (int gradientColour = 0;  gradientColour < 256;  gradientColour++){
-      stroke(gradientColour, gradientColour, gradientColour);
-      gradientLineX += (float)(width / 2) / 255;;
-      if (gradientLineX < width / 2) {
-        line(gradientLineX, height / 2, gradientLineX, height);
+    for (int intGradientColour = 0;  intGradientColour < 256;  intGradientColour++){
+      stroke(intGradientColour, intGradientColour, intGradientColour);
+      fltGradientintLineX += (float)(width / 2) / 255;;
+      if (fltGradientintLineX < width / 2) {
+        line(fltGradientintLineX, height / 2, fltGradientintLineX, height);
       }
     }
     
     // Quadrant 4: Draw Flower Pedals
     stroke(0, 0, 0);
     translate(width / 2 + width / 4 , height / 2 + height / 4);
-    for (int count = 0; count < 9; count++ ) {
+    for (int intPedalCount = 0; intPedalCount < 9; intPedalCount++ ) {
       fill(220, 93, 52);
       ellipse(0, height / 12, width / 20, height / 6);
       rotate(radians(45));
